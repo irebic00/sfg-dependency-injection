c82d4d0cf54baa13ebe079396b2d41301c8d756c
VrebiC <irebic@outlook.com>
2020-08-22 18:42:47 +0200

    ApplicationContextAware demo

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +11,16 @@ import org.springframework.stereotype.Service;
  */
 @Profile({"dog-pearson", "default"})
 @Service("petService")
-public class DogPetService implements PetService {
+public class DogPetService implements PetService, ApplicationContextAware {
+
+    private ApplicationContext ctx;
+
     public String getPetType(){
-        return "Dogs are the best!";
+        return "Dogs are the best! Psst, a secret cat has infiltrated us and says: " + ((GreetingService) ctx.getBean("i18nService")).sayGreeting();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.ctx = applicationContext;
     }
 }
